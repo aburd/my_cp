a8c77e733715988256143b3c5551a70d63e447fc
aburd <burdick.aaron@gmail.com>
2020-12-19 17:54:37 +0900

    fix bugs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,18 +3,17 @@ use std::io::Result;
 use std::path::PathBuf;
 
 fn main() -> Result<()> {
-    let (source_path, target_path, options) = match get_command() {
-        Ok(args) => args,
-        Err(e) => {
-            eprintln!("{}", e);
-            print_help();
-            std::process::exit(1);
-        }
-    };
+    let (source_path, target_path, options) = get_command()?;
 
     if options.contains(&"-R".to_owned()) {
+        if source_path.is_file() || target_path.is_file() {
+            print_invalid_usage("Both SOURCE_PATH and TARGET_PATH must be directories.");
+        }
         my_cp::copy_dir(&source_path, &target_path)?
     } else {
+        if source_path.is_dir() || target_path.is_dir() {
+            print_invalid_usage("Both SOURCE_PATH and TARGET_PATH must be files.");
+        }
         let byte_count = my_cp::copy_file(&source_path, &target_path)?;
         println!(
             "Copied {} bytes from {} to {}",
@@ -44,13 +43,12 @@ fn get_command() -> Result<(PathBuf, PathBuf, Vec<String>)> {
     Ok((source_path, target_path, options))
 }
 
-fn print_help() {
-    println!("usage: my_cp source_file target_file");
-}
-
 fn get_options() -> Option<Vec<String>> {
     let valid_options: Vec<_> = ["-R"].to_vec();
-    let options: Vec<String> = env::args().collect();
+    let options: Vec<String> = env::args()
+        .map(|a| a.to_string())
+        .filter(|s| s.starts_with('-'))
+        .collect();
 
     for option in &options {
         if !valid_options.contains(&option.as_str()) {
@@ -59,4 +57,14 @@ fn get_options() -> Option<Vec<String>> {
     }
 
     Some(options)
+}
+
+fn print_help() {
+    println!("usage: my_cp source_file target_file");
+}
+
+fn print_invalid_usage(msg: &str) {
+    println!("Invalid usage: {}", msg);
+    print_help();
+    std::process::exit(1);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,19 +8,21 @@ pub fn copy_dir(source_path: impl Into<PathBuf>, target_path: impl Into<PathBuf>
     let target_path = target_path.into();
 
     if source_path.is_file() {
-      panic!("SOURCE_PATH must be a directory");
+        panic!("SOURCE_PATH must be a directory");
     }
     if target_path.is_file() {
-      panic!("TARGET_PATH must be a directory");
+        panic!("TARGET_PATH must be a directory");
     }
 
     for entry in fs::read_dir(source_path)? {
-      let entry = entry?;
-      let path = entry.path();
-      if path.is_file() {
+        let entry = entry?;
+        let path = entry.path();
         let new_target_path = target_path.join(path.file_name().unwrap());
-        copy_file(path, new_target_path)?;
-      }
+        if path.is_file() {
+            copy_file(path, new_target_path)?;
+        } else {
+            copy_dir(path, new_target_path)?;
+        }
     }
 
     Ok(())
@@ -30,9 +32,18 @@ pub fn copy_file(
     source_path: impl Into<PathBuf>,
     target_path: impl Into<PathBuf>,
 ) -> Result<usize> {
-    let bytes = fs::read(source_path.into())?;
+    let source_path = source_path.into();
+    let target_path = target_path.into();
 
-    let target_file = File::create(target_path.into()).expect("Could not create SOURCE_FILE");
+    if source_path.is_dir() {
+        panic!("SOURCE_PATH may not be a directory when copying a file");
+    }
+    if target_path.is_dir() {
+        panic!("TARGET_PATH may not be a directory when copying a file");
+    }
+    let bytes = fs::read(source_path)?;
+
+    let target_file = File::create(target_path).expect("Could not create SOURCE_FILE");
     let mut writer = BufWriter::new(target_file);
 
     writer.write_all(&bytes)?;
