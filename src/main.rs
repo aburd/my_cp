e21d9946861fbc3590d9715313be82faa6eb7774
aburd <burdick.aaron@gmail.com>
2020-12-19 15:21:34 +0900

    add file writing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,54 @@
 use std::env;
-use std::io::Result;
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::io::{BufWriter, Result};
 use std::path::{Path, PathBuf};
-
-fn main() -> Result<()> {
-    let command = get_command()?;
-    println!("From: {:?}, To: {:?}", command.from_path, command.to_path);
-    Ok(())
-}
 
 #[derive(Debug)]
 struct Command {
-    from_path: PathBuf,
-    to_path: PathBuf,
+    source_path: PathBuf,
+    target_path: PathBuf,
+}
+
+fn main() {
+    match execute() {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("{}", e);
+            print_help();
+            std::process::exit(1);
+        }
+    }
+}
+
+fn execute() -> Result<usize> {
+    let command = get_command()?;
+    let bytes = fs::read(command.source_path)?;
+
+    let target_file = File::create(command.target_path)?;
+    let mut writer = BufWriter::new(target_file);
+    writer.write_all(&bytes[..])?;
+
+    Ok(0)
 }
 
 fn get_command() -> Result<Command> {
     let mut args = env::args();
-    let from_path: PathBuf = args
+    let source_path: PathBuf = args
         .nth(1)
-        .expect("From path is a required argument")
+        .expect("TARGET_PATH is a required argument")
         .into();
-    let to_path = match args.nth(2) {
+    let target_path = match args.nth(2) {
         Some(p) => p.into(),
         None => Path::new("./").to_path_buf(),
     };
-    // from_path
-    Ok(Command { from_path, to_path })
+    // source_path
+    Ok(Command {
+        source_path,
+        target_path,
+    })
+}
+
+fn print_help() {
+    println!("usage: my_cp source_file target_file");
 }
