37ffdec3f1931560ff156bdceead3599939ba423
aburd <burdick.aaron@gmail.com>
2020-12-19 15:42:29 +0900

    refactor into lib, refactor args

---
+++ src/bin/main.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::io::{Result};
+use std::path::{Path, PathBuf};
+use my_cp;
+
+fn main() -> Result<()> {
+    let (source_path, target_path) = get_command()?;
+    match my_cp::execute(source_path, target_path) {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            eprintln!("{}", e);
+            print_help();
+            std::process::exit(1);
+        }
+    }
+}
+
+fn get_command() -> Result<(String, String)> {
+    let mut args = env::args();
+    args.next();
+    let source_path = args
+        .next()
+        .expect("TARGET_PATH is a required argument");
+    let target_path = match args.next() {
+        Some(p) => p,
+        None => "./".to_owned(),
+    };
+    Ok((source_path, target_path))
+}
+
+fn print_help() {
+    println!("usage: my_cp source_file target_file");
+}
---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::io::{BufWriter, Result};
+use std::path::{PathBuf};
+
+pub fn execute(source_path: impl Into<PathBuf>, target_path: impl Into<PathBuf>) -> Result<usize> {
+    let bytes = fs::read(source_path.into())?;
+
+    let target_file = File::create(target_path.into())?;
+    let mut writer = BufWriter::new(target_file);
+    writer.write_all(&bytes[..])?;
+
+    Ok(0)
+}
