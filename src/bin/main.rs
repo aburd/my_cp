ba821caeef72d7d5afc4dc5644c93d1d69f3c114
aburd <burdick.aaron@gmail.com>
2020-12-19 17:03:56 +0900

    refactor to take other command

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,29 +1,56 @@
 use std::env;
-use std::io::Result;
+use std::io::{Error, ErrorKind, Result};
+use std::path::PathBuf;
 
 fn main() -> Result<()> {
-    let (source_path, target_path) = get_command()?;
-    match my_cp::execute(source_path, target_path) {
-        Ok(_) => Ok(()),
-        Err(e) => {
-            eprintln!("{}", e);
-            print_help();
-            std::process::exit(1);
-        }
-    }
+    let (source_path, target_path, options) = get_command()?;
+
+    let byte_count = if options.contains(&"-R".to_owned()) {
+        my_cp::copy_dir(&source_path, &target_path)?
+    } else {
+        my_cp::copy_file(&source_path, &target_path)?
+    };
+
+    println!(
+        "Copied {} bytes from {} to {}",
+        byte_count,
+        source_path.to_str().unwrap(),
+        target_path.to_str().unwrap(),
+    );
+
+    Ok(())
 }
 
-fn get_command() -> Result<(String, String)> {
+fn get_command() -> Result<(PathBuf, PathBuf, Vec<String>)> {
     let mut args = env::args();
     args.next();
-    let source_path = args.next().expect("TARGET_PATH is a required argument");
-    let target_path = match args.next() {
-        Some(p) => p,
-        None => "./".to_owned(),
+
+    let options: Vec<String> = get_options().unwrap_or(Vec::new());
+    let source_path: PathBuf = args
+        .next()
+        .expect("TARGET_PATH is a required argument")
+        .into();
+
+    let target_path: PathBuf = match args.next() {
+        Some(p) => p.into(),
+        None => source_path.file_name().unwrap().into(),
     };
-    Ok((source_path, target_path))
+    Ok((source_path, target_path, options))
 }
 
 fn print_help() {
     println!("usage: my_cp source_file target_file");
+}
+
+fn get_options() -> Option<Vec<String>> {
+    let valid_options: Vec<_> = ["-R"].to_vec();
+    let options: Vec<String> = env::args().collect();
+
+    for option in &options {
+        if !valid_options.contains(&option.as_str()) {
+            return None;
+        }
+    }
+
+    Some(options)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,25 @@ use std::io::prelude::*;
 use std::io::{BufWriter, Result};
 use std::path::PathBuf;
 
-pub fn execute(source_path: impl Into<PathBuf>, target_path: impl Into<PathBuf>) -> Result<usize> {
+pub fn copy_dir(source_path: impl Into<PathBuf>, target_path: impl Into<PathBuf>) -> Result<usize> {
     let bytes = fs::read(source_path.into())?;
 
-    let target_file = File::create(target_path.into())?;
+    let target_file = File::create(target_path.into()).expect("Could not create SOURCE_FILE");
     let mut writer = BufWriter::new(target_file);
-    writer.write_all(&bytes[..])?;
 
     Ok(0)
+}
+
+pub fn copy_file(
+    source_path: impl Into<PathBuf>,
+    target_path: impl Into<PathBuf>,
+) -> Result<usize> {
+    let bytes = fs::read(source_path.into())?;
+
+    let target_file = File::create(target_path.into()).expect("Could not create SOURCE_FILE");
+    let mut writer = BufWriter::new(target_file);
+
+    writer.write_all(&bytes)?;
+
+    Ok(bytes.len())
 }
